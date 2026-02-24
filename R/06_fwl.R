5b9f073d39bfb7e4b3d9543904264add0be8220a
Nikolas Kuschnig <nkuschnig@disroot.org>
2026-02-24 13:03:05 +1100

    Add DESCRIPTION, run devtools::{document,check}

--- R/06_fwl.R
+++
@@ -1,5 +0,0 @@
-#' Frisch-Waugh-Lovell projection: residualizes y and X1 on X2
-fwl <- function(y, X1, X2) {
-  Q <- qr(as.matrix(X2))
-  cbind(Yp = y - qr.fitted(Q, y), Xp = as.matrix(X1) - qr.fitted(Q, as.matrix(X1)))
-}
